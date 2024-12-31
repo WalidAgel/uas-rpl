f4a4658cd9139a48906a46385f72250643deadfc
WalidAgel <merapote11@gmail.com>
2024-12-31 16:52:16 +0700

    Update changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\HomePage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get("/", HomePage::class);
+route::get("/categories", CategoriesPage::class);
+route::get("/products", ProductsPage::class);
+route::get("/cart", CartPage::class);
